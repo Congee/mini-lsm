005ff5eea6859a0ac69d5352c2d7a35bd51a9c70
Congee <congeec@qq.com>
2023-07-22 13:32:26 -0400

    flake

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "LSM";
+
+  inputs.nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url  = "github:numtide/flake-utils";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [ rust-overlay.overlays.default
+        (final: prev: {
+          cargo = final.rust-bin.nightly.latest.default;
+          rustc = final.rust-bin.nightly.latest.default;
+        })
+      ];
+      old = import nixpkgs { inherit system; };
+      codelldb = old.vscode-extensions.vadimcn.vscode-lldb;
+      pkgs = import nixpkgs { inherit system overlays; };
+
+      # https://nixos.wiki/wiki/Nix_Cookbook#Wrapping_packages
+      debugger = pkgs.runCommand "codelldb" {} ''
+        mkdir -p $out/bin
+        ln -s ${codelldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb $out/bin
+      '';
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [  # build time
+          rustc
+          cargo
+          clippy
+          rust-analyzer
+          pkgconfig
+          buf-language-server
+          protobuf
+        ];
+        buildInputs = with pkgs; [  # run time
+          debugger
+          openssl.dev
+        ]
+        ++ lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security
+        ++ lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.AppKit
+        ;
+        RUST_SRC_PATH = "${pkgs.rust-bin.nightly.latest.rust-src}/lib/rustlib/src/rust/library";
+        # mold does not seem to use pkg-config with openssl.dev
+        # LD_LIBRARY_PATH = "${pkgs.openssl.out}/lib";  XXX: causes glibc version mismatch
+        # RUST_SRC_PATH = "${pkgs.rust.packages.nightly.rustPlatform.rustLibSrc}";
+      };
+    });
+}
